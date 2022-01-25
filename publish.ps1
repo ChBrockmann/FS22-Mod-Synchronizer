c4099cb0a7631b28d7358eacc9b3b08c575f3ea0
Christian <55225597+ChBrockmann@users.noreply.github.com>
2022-01-25 21:54:52 +0100

    Changes publishing process

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,3 @@
 rm '.\FS22-Mod-Synchronizer - Application\' -R
-dotnet publish LS-Mod-Synchronizer -o "FS22-Mod-Synchronizer - Application" -c Release
+dotnet publish LS-Mod-Synchronizer -o "FS22-Mod-Synchronizer - Application" -c Release -r win-x64 --self-contained true
 Compress-Archive -Path '.\FS22-Mod-Synchronizer - Application\*' -Destination "FS22-Mod-Synchronizer - Application" -Force
